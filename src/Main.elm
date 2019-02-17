301105bd7222ec0b41ab68109023836e6385900c
Mikolaj Klaman <mikolaj.klaman@allegro.pl>
2019-02-17 02:11:48 +0100

    use Browser.element

--- src/Main.elm
+++ src/Main.elm
@@ -1,12 +1,69 @@
 module Main exposing (main)
 
 import Browser
-import Html
+import Html exposing (Html)
+
+
+
+-- SETUP
+
+
+type alias Void =
+    ()
 
 
 main =
-    Browser.sandbox
-        { init = ()
-        , update = \_ _ -> ()
-        , view = \_ -> Html.text "Hello World"
+    Browser.element
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
         }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    Void
+
+
+init : Void -> ( Model, Cmd Msg )
+init _ =
+    ( (), Cmd.none )
+
+
+
+-- MESSAGES
+
+
+type Msg
+    = Nop
+
+
+
+-- UPDATE
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update _ _ =
+    ( (), Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view _ =
+    Html.text "Hello World"
